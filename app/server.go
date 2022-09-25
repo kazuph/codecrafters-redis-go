fc051058939bfef5a2a5f6abf5cdaa42c401f8fa
Kazuhiro Homma <kazu.homma@gmail.com>
2022-09-25 16:23:13 +0900

    handle multiple concurrent clients

--- app/server.go
+++ app/server.go
@@ -6,6 +6,21 @@ import (
 	"os"
 )
 
+func handleConnect(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		_, err := conn.Read([]byte{})
+
+		if err != nil {
+			fmt.Println("Error reading from client: ", err.Error())
+			continue
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -16,20 +31,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
 	for {
-		_, err := conn.Read([]byte{})
-
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error reading from client: ", err.Error())
-			continue
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
-		fmt.Println("Pong")
-		conn.Write([]byte("+PONG\r\n"))
+
+		go handleConnect(conn)
 	}
 }
