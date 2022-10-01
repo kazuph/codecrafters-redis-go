1222a6efa2dc3df0896dc2b7add7c6183b1fca4b
Kazuhiro Homma <kazu.homma@gmail.com>
2022-10-01 17:56:27 +0900

    test

--- app/server.go
+++ app/server.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -31,15 +30,16 @@ func main() {
 func handleConnect(conn net.Conn, mem *Mem) {
 	defer conn.Close()
 
-	msg := make([]byte, 1024)
-	_, err := conn.Read(msg)
-	if err != nil {
-		fmt.Println("Error reading: ", err.Error())
-	}
-	fmt.Println("msg: ", string(msg))
+	// msg := make([]byte, 1024)
+	// _, err := conn.Read(msg)
+	// if err != nil {
+	// 	fmt.Println("Error reading: ", err.Error())
+	// }
+	// fmt.Println("msg: ", string(msg))
 
 	for {
-		value, err := DecodeRESP(bufio.NewReader(strings.NewReader(string(msg))))
+		// value, err := DecodeRESP(bufio.NewReader(strings.NewReader(string(msg))))
+		value, err := DecodeRESP(bufio.NewReader(conn))
 
 		if err != nil {
 			fmt.Println("Error reading from client: ", err.Error())
