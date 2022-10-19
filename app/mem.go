a1194b2b232cd07c98e498c92eaac29c5c67892f
Kazuhiro Homma <kazu.homma@gmail.com>
2022-10-19 16:17:57 +0900

    test

--- app/mem.go
+++ app/mem.go
@@ -29,7 +29,7 @@ func (m *Mem) Get(key string) (string, bool) {
 		return "", false
 	}
 
-	if !valueWithExpiry.expiresAt.IsZero() && time.Now().Before(valueWithExpiry.expiresAt) {
+	if !valueWithExpiry.expiresAt.IsZero() && time.Now().After(valueWithExpiry.expiresAt) {
 		delete(m.data, key)
 		log.Println("GET: expired")
 		return "", false
